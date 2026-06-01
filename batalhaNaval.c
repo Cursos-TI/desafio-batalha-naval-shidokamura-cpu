1831f1b9aa0357903184f519153f4c31007d3b32
Matheus <shidokamura@gmail.com>
2026-06-01 01:49:38 +0000

    Desafio Batalha naval Primeira parte feita.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,10 +6,42 @@
 
 int main() {
     // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
+    int tabuleiro[10][10] = {0}; // Inicializa o Tabuleiro de 10x10 com 0 (água).
+    char coluna[10] = {1,2,3,4,5,6,7,8,9,10};
+    int linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
+ 
+    printf("Tabuleiro de Batalha Naval\n");
+    printf(" * ");
+    for (int l = 0; l < 10; l++) { // impressão da linha do tabuleiro 
+        printf("%d ", coluna[l]);      
+    }
+    for (int l = 0; l < 10; l++) { //impressão da coluna do tabuleiro
+        printf("\n %c ", linha[l]);
+        for (int c = 0; c < 10; c++) {
+            printf("%d ", tabuleiro[l][c]);
+        }
+    }
+    //spawn dos navios 
+    tabuleiro[2][3] = 3; // começo do navio
+    tabuleiro[2][4] = 3; // meio do navio
+    tabuleiro[2][5] = 3; // fim do navio
+    
+    tabuleiro[5][7] = 3; 
+    tabuleiro[6][7] = 3;  
+    tabuleiro[7][7] = 3; 
+    // Imprime o tabuleiro atualizado com os navios
+    printf("\n\nTabuleiro atualizado com navios:\n");
+    printf(" * ");
+    for (int l = 0; l < 10; l++) {
+        printf("%d ", coluna[l]);      
+    }
+    for (int l = 0; l < 10; l++) {
+        printf("\n %c ", linha[l]);
+        for (int c = 0; c < 10; c++) {
+            printf("%d ", tabuleiro[l][c]);
+        }
+    }
+    printf("\n");
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
